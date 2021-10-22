6c69ddcf0396bba30a6438109c68726c16975971
Abner Joia <t2m.ajoia@terceiros.mag.com.br>
2021-10-21 22:46:49 -0300

    Aula-06

---
+++ Aulas/Exemplos/Javascript/assets/js/array.js
@@ -0,0 +1,32 @@
+const variosTipos = ["Mamona", 10, [1,2]];
+console.log(variosTipos[2][1]);
+variosTipos[3] = 15;
+variosTipos[10] = 100;
+console.log(variosTipos);
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+console.log(array.length);
+array.push("push");
+array.shift();
+array.pop();
+
+var localizado = array.indexOf(4);
+const valor = array.find(elemento => elemento === 3);
+console.log(valor);
+
+var arrayString = array.join('');
+console.log(arrayString);
+
+
+
+//console.log(localizado);
+
+//console.log(array);
+
+if(10 === '10'){
+    console.log('Verdadeiro')
+}
+else{
+    console.log('Falso')
+}
---
+++ Aulas/Exemplos/Javascript/assets/js/script.js
@@ -0,0 +1,24 @@
+/*var nome = "Abner";
+var frase = `${nome} é um cara legal`;
+
+console.log(frase);
+
+const pessoa = {
+    nome: "Abner",
+    idade: 30,
+    endereco: {
+        cidade: "Petrópolis",
+        estado: "Rio de Janeiro"
+    },
+    gritar: function(){
+        console.log("AAAAAAAAAAAAAAA")
+    }
+}
+
+pessoa.nome = "João";
+console.log(pessoa.nome);
+console.log(pessoa.endereco.cidade);
+var minhaFuncao = pessoa.gritar;
+minhaFuncao();
+console.log(pessoa);*/
+
